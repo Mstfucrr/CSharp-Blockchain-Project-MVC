3200b09fb92beb7e24f947a6e30199f19261ac01
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-08 17:05:56 +0300

    Proje dosyası ekle.

---
+++ RecycleCoinMvc/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecycleCoinMvc.Models;
+
+namespace RecycleCoinMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            Console.WriteLine(Session["User"]);
+            ViewData["User"] = Session["User"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var nameValueCollection = Request.Form;
+            var user = new User().Login(nameValueCollection["username"], nameValueCollection["password"]);
+            if (user.Id == 0)
+            {
+                Console.WriteLine("Giriş Hatası");
+            }
+            else
+            {
+                Session.Add("User", user);
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public ActionResult Register()
+        {
+            var nameValueCollection = Request.Form;
+            var user = new User();
+            user.Name = nameValueCollection["Name"];
+            user.Password = nameValueCollection["password"];
+            user.Email = nameValueCollection["email"];
+            user.Lastname = nameValueCollection["Lastname"];
+            user.Username = nameValueCollection["username"];
+
+
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("User");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ RecycleCoinMvc/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecycleCoinMvc.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public User Login(string username, string password)
+        {
+            if (username == "mstf" && password == "mm123")
+            {
+                Id = 1;
+                Name = "Mustafa";
+                Lastname = "Uçar";
+                Username = username;
+                Email = "mm@gmail.com";
+
+            }
+            else
+            {
+                Id = 0;
+            }
+
+            return this;
+        }
+    }
+}
