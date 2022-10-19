abc1d719a5a05232f3f37bde63bece1acc293367
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-19 17:42:48 +0300

    Update Modle

--- RecycleCoinMvc/Models/User.cs
+++ RecycleCoinMvc/Models/User.cs
@@ -7,32 +7,11 @@ namespace RecycleCoinMvc.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
+        public string RoleName { get; set; }
 
-
-        public User Login(string username, string password)
-        {
-            if (username == "mstf" && password == "mm123")
-            {
-                Id = 1;
-                Name = "Mustafa";
-                Lastname = "Uçar";
-                Username = username;
-                Email = "mm@gmail.com";
-
-            }
-            else
-            {
-                Id = 0;
-            }
-
-            return this;
-        }
     }
 }
