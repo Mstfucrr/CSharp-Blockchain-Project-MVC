79fdb85eecb6d75c8c66de55b4c104a3e8388ecd
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-19 17:42:34 +0300

    Update controller

--- RecycleCoinMvc/Controllers/UserController.cs
+++ RecycleCoinMvc/Controllers/UserController.cs
@@ -17,34 +17,39 @@ namespace RecycleCoinMvc.Controllers
             return View();
         }
 
-        [HttpPost]
+
         public ActionResult Login()
         {
+            var method = Request.HttpMethod;
+            if (method == "POST")
+            {
+                return RedirectToAction("Index");
+            }
+            Session.Clear();
+            return View();
 
-            var nameValueCollection = Request.Form; //Login formdan giriş bilgilerini çeker.
-            var user = new User().Login(nameValueCollection["username"], nameValueCollection["password"]);
-            if (user.Id == 0)
-                Console.WriteLine("Giriş Hatası");
-            else
-                Session.Add("User", user);
 
-            return RedirectToAction("Index");
 
         }
-        [HttpPost]
+
         public ActionResult Register()
         {
-            var nameValueCollection = Request.Form;
-            var user = new User();
-            user.Name = nameValueCollection["Name"];
-            user.Password = nameValueCollection["password"];
-            user.Email = nameValueCollection["email"];
-            user.Lastname = nameValueCollection["Lastname"];
-            user.Username = nameValueCollection["username"];
+            if (Request.HttpMethod == "POST")
+            {
+                var nameValueCollection = Request.Form;
+                var user = new User
+                {
+                    Name = nameValueCollection["regName"],
+                    Lastname = nameValueCollection["regLastname"],
+                    Username = nameValueCollection["regUsername"],
+                    Password = nameValueCollection["regPassword"]
+                };
 
+                return RedirectToAction("Index");
+            }
+            
+            return View();
 
-
-            return RedirectToAction("Index");
         }
 
         public ActionResult Logout()
