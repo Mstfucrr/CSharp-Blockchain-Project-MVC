b3f13959a4eecc1449d520425fbddbafd18360ff
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-19 21:02:10 +0300

    add transaction controller

---
+++ RecycleCoinMvc/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecycleCoinMvc.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult CreateTransaction()
+        {
+            return View();
+        }
+
+        public ActionResult PendingTransaction()
+        {
+            return View();
+        }
+
+    }
+}
