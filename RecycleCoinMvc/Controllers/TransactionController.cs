e1a333f772942de31ccb2d1ca3b435d11e74b007
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-22 00:04:40 +0300

    add CreateTransaction

--- RecycleCoinMvc/Controllers/TransactionController.cs
+++ RecycleCoinMvc/Controllers/TransactionController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace RecycleCoinMvc.Controllers
 {
@@ -18,10 +20,29 @@ namespace RecycleCoinMvc.Controllers
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("http://localhost:5000");
         }
-        
+
         // GET: Transaction
         public ActionResult CreateTransaction()
         {
+            if (Request.HttpMethod == "POST")
+            {
+
+                var fromAddress = Request.Form["fromAddress"];
+                var toAddress = Request.Form["toAddress"];
+                var amount = Request.Form["amount"];
+                var transactionContent = new JObject(new JProperty("fromAddress", fromAddress),
+                        new JProperty("toAddress", toAddress),
+                        new JProperty("amount", amount)).ToString();
+
+                var content = new StringContent(transactionContent, System.Text.Encoding.UTF8, "application/json");
+
+                var pushTransaction = httpClient.PostAsync("api/Transaction/AddTransaction", content);
+                pushTransaction.Result.EnsureSuccessStatusCode();
+                return Json("Transaction is : "  + transactionContent);
+
+            }
+
+
             return View();
         }
 
@@ -35,7 +56,7 @@ namespace RecycleCoinMvc.Controllers
         public ActionResult GetTransactionByBlock(string hash)
         {
             Session.Clear();
-            var getBlockchain = httpClient.GetAsync("api/getBlockByHash/"+hash);
+            var getBlockchain = httpClient.GetAsync("api/Blockchain/getBlockByHash/" + hash);
             getBlockchain.Result.EnsureSuccessStatusCode();
             var res = getBlockchain.Result.Content.ReadAsStringAsync().Result;
             var j_res = JsonConvert.DeserializeObject(res);
