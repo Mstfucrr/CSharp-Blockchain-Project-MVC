4cbda64243b4e8bcb289ea3ec87a57efa00c71f6
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-10-26 23:11:46 +0300

    add Get by filter to IEntityRepositoryBase

--- RecycleCoin.DataAccess/Abstract/IEntityRepositoryBase.cs
+++ RecycleCoin.DataAccess/Abstract/IEntityRepositoryBase.cs
@@ -8,6 +8,7 @@ namespace RecycleCoin.DataAccess.Abstract
     public interface IEntityRepositoryBase<T> where T : class, IEntity, new()
     {
         List<T> GetList(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter = null);
         void Add(T entity);
         void Delete(T entity);
         void Update(T entity);
