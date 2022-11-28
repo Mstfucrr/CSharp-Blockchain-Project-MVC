851f5afe90161a2864c905e8b9d460b135d6e159
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-11-29 00:58:16 +0300

    add get User and User's recycle item infos for admin

---
+++ RecycleCoinMvc/Areas/Admin/Controllers/ShowUsersController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using RecycleCoin.Business.Concrete;
+using System.Web.Mvc;
+using RecycleCoin.DataAccess.Concrete.EntityFramework;
+using RecycleCoin.Entities.Concrete;
+
+namespace RecycleCoinMvc.Areas.Admin.Controllers
+{
+    public class ShowUsersController : Controller
+    {
+        private readonly UserRecycleItemManager _userRecycleItemManager;
+
+        public ShowUsersController()
+        {
+            _userRecycleItemManager = new UserRecycleItemManager(new EfUserRecycleItemDal());
+        }
+        // GET: Admin/ShowUsers
+        public ActionResult Index()
+        {
+            var userRecycleItemInfos = _userRecycleItemManager.GetList();
+            
+            ViewBag.UserRecycleItemInfos = userRecycleItemInfos;
+            return View();
+        }
+    }
+}
