6ec65f57204186070dbd6bc25bc62035a50b4bfb
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-11-30 16:21:39 +0300

    add GetListOrderByDate and desc

--- RecycleCoin.DataAccess/Concrete/EntityFramework/EfUserRecycleItemDal.cs
+++ RecycleCoin.DataAccess/Concrete/EntityFramework/EfUserRecycleItemDal.cs
@@ -9,11 +9,30 @@ namespace RecycleCoin.DataAccess.Concrete.EntityFramework
     public class EfUserRecycleItemDal:EfEntityRepositoryBase<UserRecycleItem>, IUserRecycleItemDal
     {
         private RecycleCoinDbContext _recycleCoinDbContext;
-        public List<UserRecycleItem> GetListByUser(string userId)
+
+        public EfUserRecycleItemDal()
         {
             _recycleCoinDbContext = new RecycleCoinDbContext();
+
+        }
+        public List<UserRecycleItem> GetListByUser(string userId)
+        {
            var userRecycleItems = new List<UserRecycleItem>(_recycleCoinDbContext.UserRecycleItems.SqlQuery($@"call recyclecoindb.sp_getUserRecycleItemsByUserId('{userId}');"));
            return userRecycleItems ?? null;
         }
+
+        public List<UserRecycleItem> GetListOrderByDate()
+        {
+            var userRecycleItems = new List<UserRecycleItem>(_recycleCoinDbContext.UserRecycleItems.SqlQuery($@"Select * from recyclecoindb.userrecycleitems ur order by ur.RecycleDate;"));
+            return userRecycleItems ?? null;
+        }
+
+        public List<UserRecycleItem> GetListOrderByDateDesc()
+        {
+            var userRecycleItems = new List<UserRecycleItem>(_recycleCoinDbContext.UserRecycleItems.SqlQuery($@"Select * from recyclecoindb.userrecycleitems ur order by ur.RecycleDate desc;"));
+            return userRecycleItems ?? null;
+        }
+
+
     }
 }
