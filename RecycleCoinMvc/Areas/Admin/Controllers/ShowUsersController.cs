d0ae771d39090945930e68b4e3bf0dfe409a0a7b
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-12-02 03:56:51 +0300

    add GetUserInfo card for clicked user

--- RecycleCoinMvc/Areas/Admin/Controllers/ShowUsersController.cs
+++ RecycleCoinMvc/Areas/Admin/Controllers/ShowUsersController.cs
@@ -1,19 +1,27 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using RecycleCoin.Business.Concrete;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using RecycleCoin.DataAccess.Concrete.EntityFramework;
 using RecycleCoin.Entities.Concrete;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RecycleCoin.DataAccess.Concrete.EntityFramework.Contexts;
 
 namespace RecycleCoinMvc.Areas.Admin.Controllers
 {
     public class ShowUsersController : Controller
     {
         private readonly UserRecycleItemManager _userRecycleItemManager;
+        private readonly UserManager<AppUser> _userManager;
+
 
         public ShowUsersController()
         {
             _userRecycleItemManager = new UserRecycleItemManager(new EfUserRecycleItemDal());
+            var userStore = new UserStore<AppUser>(new RecycleCoinDbContext());
+
+            _userManager = new UserManager<AppUser>(userStore);
+
         }
         // GET: Admin/ShowUsers
         public ActionResult Index()
@@ -37,6 +45,13 @@ namespace RecycleCoinMvc.Areas.Admin.Controllers
             var userRecycleItemInfos = _userRecycleItemManager.GetListOrderByDesc(filter);
             ViewBag.UserRecycleItemInfos = userRecycleItemInfos;
             return PartialView("_userRecycleItemsPartial");
+        }
+
+        public ActionResult GetUserInfo(string userId)
+        {
+            var user = _userRecycleItemManager.GetList().First(u=>u.User.Id == userId).User;
+            ViewBag.User = user;
+            return PartialView("_userInfoPartial");
         }
     }
 }
