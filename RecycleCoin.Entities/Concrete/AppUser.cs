24dd3b05b59a7683b8ce917af28e449f4090af6d
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2022-12-08 23:47:54 +0300

    add TcNo and year columns(properries) for check TC persons

--- RecycleCoin.Entities/Concrete/AppUser.cs
+++ RecycleCoin.Entities/Concrete/AppUser.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace RecycleCoin.Entities.Concrete
 {
     public class AppUser:IdentityUser
     {
+        public long TcNo { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
+        public int Year { get; set; }
         public string PublicKey { get; set; }
         public string PrivateKey { get; set; }
         public int Carbon { get; set; }
